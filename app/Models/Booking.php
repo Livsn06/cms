20cfc5c9ee1f98629deb96a0748bc64a04752752
Jmgty <joelgutlay04@gmail.com>
2026-03-16 22:59:37 +0800

    add table

---
+++ app/Models/Booking.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Booking extends Model
+{
+    protected $table = 'bookings';
+
+    protected $fillable = [
+        'package_id',
+        'phone',
+        'event_date',
+        'guest_count',
+        'status',
+    ];
+
+
+
+    public function package()
+    {
+        return $this->belongsTo(Package::class, 'package_id');
+    }
+}
