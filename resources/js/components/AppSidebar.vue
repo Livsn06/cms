4349794a02f5fa57cc86bece75ae4662cd3490ad
Jmgty <joelgutlay04@gmail.com>
2026-03-16 20:28:10 +0800

    add components

--- resources/js/components/AppSidebar.vue
+++ resources/js/components/AppSidebar.vue
@@ -1,6 +1,6 @@
 <script setup lang="ts">
 import { Link } from '@inertiajs/vue3';
-import { BookOpen, FolderGit2, LayoutGrid } from 'lucide-vue-next';
+import { BookOpen, FolderGit2, LayoutGrid, Package } from 'lucide-vue-next';
 import AppLogo from '@/components/AppLogo.vue';
 import NavFooter from '@/components/NavFooter.vue';
 import NavMain from '@/components/NavMain.vue';
@@ -26,7 +26,7 @@ const mainNavItems: NavItem[] = [
     {
         title: 'Packages',
         href: packages(),
-        icon: LayoutGrid,
+        icon: Package,
     },
 ];
 
