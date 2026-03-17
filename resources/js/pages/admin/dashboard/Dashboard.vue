1047e5e93aadf8d081dbcb1d9a0ef9b67aef529d
Jmgty <joelgutlay04@gmail.com>
2026-03-17 21:11:43 +0800

    add data ui

--- resources/js/pages/admin/dashboard/Dashboard.vue
+++ resources/js/pages/admin/dashboard/Dashboard.vue
@@ -4,6 +4,7 @@ import PlaceholderPattern from '@/components/PlaceholderPattern.vue';
 import AppLayout from '@/layouts/AppLayout.vue';
 import { dashboard } from '@/routes/admin';
 import type { BreadcrumbItem } from '@/types';
+import CustomStatCard from '@/components/CustomStatCard.vue';
 
 const breadcrumbs: BreadcrumbItem[] = [
     {
@@ -11,6 +12,16 @@ const breadcrumbs: BreadcrumbItem[] = [
         href: dashboard(),
     },
 ];
+
+
+const props = defineProps<{
+    stats: {
+        totalBookings: number;
+        activePackages: number;
+        totalRevenue: string;
+        bookingTrend: string;
+    }
+}>();
 </script>
 
 <template>
@@ -19,19 +30,17 @@ const breadcrumbs: BreadcrumbItem[] = [
 
     <AppLayout :breadcrumbs="breadcrumbs">
         <div class="flex h-full flex-1 flex-col gap-4 overflow-x-auto rounded-xl p-4">
-            <div class="grid auto-rows-min gap-4 md:grid-cols-3">
-                <div
-                    class="relative aspect-video overflow-hidden rounded-xl border border-sidebar-border/70 dark:border-sidebar-border">
-                    <PlaceholderPattern />
-                </div>
-                <div
-                    class="relative aspect-video overflow-hidden rounded-xl border border-sidebar-border/70 dark:border-sidebar-border">
-                    <PlaceholderPattern />
-                </div>
-                <div
-                    class="relative aspect-video overflow-hidden rounded-xl border border-sidebar-border/70 dark:border-sidebar-border">
-                    <PlaceholderPattern />
-                </div>
+            <div class="grid gap-4 md:grid-cols-3">
+
+                <CustomStatCard title="Total Bookings" :value="stats.totalBookings" trend="-20%" trendType="down"
+                    label="Down 20% this period" description="Acquisition needs attention" />
+
+                <CustomStatCard title="Active Packages" :value="stats.activePackages" trend="+5%" trendType="up"
+                    label="Growing library" description="Up from last month" />
+
+                <CustomStatCard title="Expected Revenue" :value="'$' + stats.totalRevenue" label="Total value"
+                    description="Pending + Confirmed" />
+
             </div>
             <div
                 class="relative min-h-[100vh] flex-1 rounded-xl border border-sidebar-border/70 md:min-h-min dark:border-sidebar-border">
